66de973c0b15f9f2ce8932d1d0522ffb5481ed64
lucas-tadeu-dev <lucastmb2016@gmail.com>
2022-07-17 11:46:21 -0300

    second commit

--- library/settings.py
+++ library/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3lg7g(a!^q85&nb)u6_t8za5x1)r$jpxilkutar7e-e5yb=4et'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
